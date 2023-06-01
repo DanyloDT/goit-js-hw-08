e9c3f04b06b175a9e04801da1236a2fb5370bb74
Danylo Diachenko <danylodiachenko@gmail.com>
2023-06-01 17:01:00 +0300

    правки

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+// import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+
+const makeItemCard = ({preview, original, description} = {}) => {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" title="${description}" />
+        </a>
+    </li>`;
+};
+
+const makeCardArr = galleryItems.map(imgInfo => {
+  return makeItemCard(imgInfo)
+})
+
+gallery.innerHTML = makeCardArr.join("")
+
+new SimpleLightbox('.gallery a', {captions: true, captionDelay: 250});
