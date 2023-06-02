60b416979136645290dc42ff4dbaded678dcea0e
Danylo Diachenko <danylodiachenko@gmail.com>
2023-06-02 22:48:44 +0300

    commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import storage from "./storag.js"
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+    storage.save("videoplayer-current-time", event.seconds);
+    console.log(event);
+}
+
+player.on('timeupdate', throttle(onPlay, 1000))
+
+
+const time = storage.load("videoplayer-current-time")
+
+if (time) {
+    player.setCurrentTime(time)
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import storage from "./storag.js"
+const throttle = require('lodash.throttle');
+
+const formEl = document.querySelector('.feedback-form')
+userInfo = {}
+
+const infoFormFields = () => {
+    const infoLS = storage.load('feedback-form-state')
+
+    if (infoLS === undefined) {
+        return
+    }
+
+    // console.log(infoLS);
+    for (const key in infoLS) {
+        formEl.elements[key].value = infoLS[key];
+    }
+}
+
+infoFormFields()
+
+const inputFormEl = event => {
+    const infoEl = event.target
+    const infoValue = infoEl.value
+    const infoName = infoEl.name
+    userInfo[infoName] = infoValue
+    
+    storage.save('feedback-form-state', userInfo)
+}
+
+const onSubmit = event => {
+    event.preventDefault()
+
+    const infoEl = event.target
+    const { email, message } = infoEl.elements
+
+    if (email.value === "" || message.value === "") {
+        return
+    }
+
+    infoEl.reset()
+    localStorage.clear()
+    console.log(userInfo);
+}
+
+formEl.addEventListener('input', throttle(inputFormEl, 500))
+formEl.addEventListener('submit', onSubmit)
 
